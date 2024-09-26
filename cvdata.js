a2065064a622f52b09a8f9d1afe9842a0e341c69
Jorge Benjumea <me@intron014.com>
2024-09-26 09:46:17 +0200

    API :D

--- cvdata.js
+++ cvdata.js
@@ -12,17 +12,25 @@ const cvData = {
         {
           title: 'Reldus',
           description: 'Developed a chess engine in Swift that leverages an AI decision tree with alpha-beta pruning to predict optimal moves. ' + 
-                      'The engine communicates using the standard [UCI protocol](https://www.chessprogramming.org/UCI) and supports advanced chess rules including castling, en passant, threefold repetition, and pawn promotion.',
+                       'The engine communicates using the standard [UCI protocol](https://www.chessprogramming.org/UCI) and supports advanced chess rules including castling, en passant, threefold repetition, and pawn promotion.',
           link: 'https://github.com/Intron014/Reldus',
-          tags: ['Swift', 'AI', 'Chess']
+          tags: ['Swift', 'Machine Learning', 'Chess']
         },
         {
           title: 'Gasoprice',
           description: 'Built a PWA that fetches real-time gas prices for all Spanish gas stations and then displays the closest ones to the user, ' +
-                      'allowing for fine-grained filtering by fuel type, brand and prices. It also allows users to see a map view of the stations.',
+                       'allowing for fine-grained filtering by fuel type, brand and prices. It also allows users to see a map view of the stations.',
           link: 'https://gasoprice.com',
           tags: ['PWA', 'Flask', 'Python', 'API']
-        }
+        },
+		{
+		  title: 'API',
+		  description: 'Developed using the [Vapor](https://vapor.codes/) framework an API to replace my old inefficient Flask API, with various endpoints ' +
+					   'such as: Retrieving bus times, fetching my favorite stations, grabbing the song I am currently listening to and a few other more ' +
+					   '(such as serving the .js used to display this very list)',
+		  link: 'https://api.intron014.com',
+		  tags: ['Swift', 'API', 'Vapor']
+		}
     ]
 };
 window.addEventListener('load', () => populateCV(cvData));
